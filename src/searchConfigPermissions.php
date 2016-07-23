755572eeaa93f3df6ac06066df1de4c83fc8b1be
naveenvalecha <naveenvalecha@2665733.no-reply.drupal.org>
2016-07-23 16:04:55 +0530

    Issue #2771569 by naveenvalecha: Fix the coding standard issues

--- src/searchConfigPermissions.php
+++ src/searchConfigPermissions.php
@@ -1,20 +1,19 @@
 <?php
 
-/*
- * @file
- *
- * contains \Drupal\search_config\searchConfigPermissions
- */
+namespace Drupal\search_config;
 
-namespace  Drupal\search_config;
+/**
+ * Provides dynamic search permissions for nodes of different types.
+ */
 class searchConfigPermissions {
-    public function permissions() {
-        $permissions = [];
-        foreach (search_config_content_types() as $type => $label) {
-            $permissions["search $type content"] = array(
-                'title' => t('%type_name: Search content of this type', array('%type_name' => $label)),
-            );
-        }
-        return $permissions;
+
+  public function permissions() {
+    $permissions = [];
+    foreach (search_config_content_types() as $type => $label) {
+      $permissions["search $type content"] = array(
+        'title' => t('%type_name: Search content of this type', array('%type_name' => $label)),
+      );
     }
+    return $permissions;
+  }
 }
--- tests/src/Unit/permissionsTest.php
+++ tests/src/Unit/permissionsTest.php
@@ -4,16 +4,18 @@
  * Testing the permission()
  */
 use \Drupal\search_config\searchConfigPermissions;
-class permissionsTest extends PHPUnit_Framework_TestCase {
-    public $permissionSet;
-    public function setUp()
-    {
-        $this->permissionSet = new searchConfigPermissions();
-    }
 
-    public function testPermissions() {
-        $this->assertEquals('Article: Search content of this type',$this->permissionSet["search Article content"]->searchConfigPermissions->permissions());
-        $this->assertEquals('Article: Search content of this type',$this->permissionSet["search Page content"]->searchConfigPermissions->permissions());
-    }
+class permissionsTest extends PHPUnit_Framework_TestCase {
+  public $permissionSet;
+
+  public function setUp() {
+    $this->permissionSet = new searchConfigPermissions();
+  }
+
+  public function testPermissions() {
+    $this->assertEquals('Article: Search content of this type', $this->permissionSet["search Article content"]->searchConfigPermissions->permissions());
+    $this->assertEquals('Article: Search content of this type', $this->permissionSet["search Page content"]->searchConfigPermissions->permissions());
+  }
 }
+
 ?>
