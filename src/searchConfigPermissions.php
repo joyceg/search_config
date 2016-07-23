e60ad5dea1cc594f4b5d04a48977cf5c99e60b18
naveenvalecha <naveenvalecha@2665733.no-reply.drupal.org>
2016-07-23 16:30:09 +0530

    Issue #2771581 by naveenvalecha: use NodeType::loadMultiple instead of search_config_content_types in searchConfigPermissions.php

--- src/searchConfigPermissions.php
+++ src/searchConfigPermissions.php
@@ -2,18 +2,33 @@
 
 namespace Drupal\search_config;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\node\Entity\NodeType;
+
 /**
  * Provides dynamic search permissions for nodes of different types.
  */
 class searchConfigPermissions {
 
-  public function permissions() {
-    $permissions = [];
-    foreach (search_config_content_types() as $type => $label) {
-      $permissions["search $type content"] = array(
-        'title' => t('%type_name: Search content of this type', array('%type_name' => $label)),
+  use StringTranslationTrait;
+
+  /**
+   * Returns an array of search config permissions.
+   *
+   * @return array
+   *   The search config permissions.
+   *   @see \Drupal\user\PermissionHandlerInterface::getPermissions()
+   */
+  public function searchConfigPermissions() {
+    $perms = array();
+    // Generate search config permissions for all node types.
+    foreach (NodeType::loadMultiple() as $type) {
+      $type_id = $type->id();
+      $perms["search $type_id content"] = array(
+        'title' => $this->t('%type_name: Search content of this type', array('%type_name' => $type->label())),
       );
     }
-    return $permissions;
+
+    return $perms;
   }
 }
